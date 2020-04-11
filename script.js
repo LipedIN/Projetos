038b9f893e99fb945d3baff8b5bcccb7e0cdca23
LipedIN <63268358+LipedIN@users.noreply.github.com>
2020-04-11 18:46:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-function carregar() {
-    var msg = window.document.getElementById('mensagem')
-    var foto = window.document.getElementById('image')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.` 
-    if (hora >= 5 && hora <12){
-        // bom dia
-        foto.src="fotomanha.png"
-        document.body.style.background = '#f5c55d'
-    } else if (hora >= 12 && hora <= 18){
-        // boa tarde
-        foto.src="fototarde.png"
-        document.body.style.background = '#b7ba00'
-    } else {
-        // boa noite
-        foto.src="fotonoite.png"
-        document.body.style.background = '#191970'
-    }
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = " ";
+    textoArray.forEach((letra, i) => {
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    });
+    console.log(textoArray);
 }
+    const titulo = document.querySelector('h1');
+    typeWriter(titulo);
